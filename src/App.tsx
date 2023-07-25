74aca9bb2ffc3556e2b6d7537ffc231f9ac3d711
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2023-07-25 17:00:55 -0400

    feat: added personal-ui-package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Button, Panel } from "@cjrojasb/personal-ui-package";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline text-blue-300">Hello world!</h1>;
+  return (
+    <Panel className="bg-white">
+      <h1 className="text-3xl font-bold underline text-blue-300">
+        Hello world!
+      </h1>
+      <Button variation="primary" className="p-5">
+        Primary
+      </Button>
+    </Panel>
+  );
 }
 
 export default App;
